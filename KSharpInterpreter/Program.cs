0dd5ca489ea2fd42b2df442884e9f121b93b1f74
g-bunny <miri@newschool.edu>
2019-11-18 14:48:12 -0500

    skeletal structures in setting up interpreter

--- KSharpInterpreter/Program.cs
+++ KSharpInterpreter/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace KSharpInterpreter
-{
-    class MainClass
-    {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace KSharpInterpreter {
+    //sample input to interpret:
+    //func AddIfNotSame(num a, num b){
+    //  if(equals(a,b)){
+    //      return "same"
+    //  }
+    //  return plus(a,b)
+    //var a = AddIfNotSame(3,4)
+    //var b = AddIfNotSame(4,5)
+    //return plus(a,b)
+    //esc
+    class MainClass {
+        public static void Main (string[] args) {
+            Console.WriteLine ("Write your code here! Press 'enter' + 'esc' when you are ready to compile");
+            Syntax Kyntax = new Syntax ();
+            ConsoleInterface Kinterface = new ConsoleInterface ();
+            SimpleParse KimpleKarse = new SimpleParse ();
+            List<string> enteredString = new List<string> ();
+
+            //now we must parse into tokens
+            //enteredString = Kinterface.TakeInput ();
+            //string[] split = KimpleKarse.splitIntoIndiv (enteredString[0]);
+            //Console.WriteLine (string.Join (", ", split));
         }
+    }
+
+    class ConsoleInterface {
+
+    }
+    class SimpleParse {
+
+    }
+    class Lexer {
+
+    }
+    class Syntax {
+
     }
 }
